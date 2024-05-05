01980ddc6ad925fc8501c56d285d1fac3dcbf7a8
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-05-05 19:44:52 +0100

    US001 - RegisterSkillController Documentation.

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterSkillController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/RegisterSkillController.java
@@ -11,11 +11,18 @@ public class RegisterSkillController {
 
     private SkillRepository skillRepository;
 
-    //Repository instance is obtained from the Repositories class
+    /**
+     * Constructs a new RegisterSkillController and obtains a SkillRepository instance from Repositories class.
+     */
     public RegisterSkillController() {
         this.skillRepository = getSkillRepository();
     }
 
+    /**
+     * Gets the SkillRepository instance from Repositories class.
+     *
+     * @return The SkillRepository instance.
+     */
     private SkillRepository getSkillRepository() {
         if (skillRepository == null) {
             Repositories repositories = Repositories.getInstance();
@@ -25,11 +32,34 @@ public class RegisterSkillController {
         return skillRepository;
     }
 
+    /**
+     * Registers a new skill with the given name.
+     *
+     * @param skillName The name of the skill to register.
+     * @return An Optional containing the registered Skill if successful, or empty otherwise.
+     */
     public Optional<Skill> registerSkill(String skillName) {
 
         Optional<Skill> newSkill;
 
         newSkill = skillRepository.registerSkill(skillName);
+
+        return newSkill;
+
+    }
+
+    /**
+     * Registers a new skill with the given name and description.
+     *
+     * @param skillName   The name of the skill to register.
+     * @param description The description of the skill.
+     * @return An Optional containing the registered Skill if successful, or empty otherwise.
+     */
+    public Optional<Skill> registerSkill(String skillName,String description) {
+
+        Optional<Skill> newSkill;
+
+        newSkill = skillRepository.registerSkill(skillName,description);
 
         return newSkill;
 
