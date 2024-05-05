92e6fde8dbe6d16dfd3faf8bb0b648d1b9e8b3c6
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-05-05 20:42:40 +0100

    US001 - RegisterSkillUI Documentation and add description for skill.

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterSkillUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterSkillUI.java
@@ -3,46 +3,118 @@ package pt.ipp.isep.dei.esoft.project.ui.console;
 import pt.ipp.isep.dei.esoft.project.application.controller.RegisterSkillController;
 import pt.ipp.isep.dei.esoft.project.domain.Skill;
 
+import java.util.InputMismatchException;
 import java.util.Optional;
 import java.util.Scanner;
 
 
-public class RegisterSkillUI {
+public class RegisterSkillUI implements Runnable{
 
     private final RegisterSkillController controller;
     private String skillName;
+    private String skillDescription;
 
-
-    public RegisterSkillUI(){
-         controller = new RegisterSkillController();
+    /**
+     * Default constructor that initializes the RegisterSkillController.
+     */
+    public RegisterSkillUI() {
+        controller = new RegisterSkillController();
     }
 
-    private RegisterSkillController getController(){
+    /**
+     * Gets the RegisterSkillController instance.
+     *
+     * @return The RegisterSkillController instance.
+     */
+    private RegisterSkillController getController() {
         return controller;
     }
 
+    /**
+     * Main method that executes the skill registration interface.
+     */
     public void run() {
         System.out.println("\n\n--- Register Skill ------------------------");
 
+        int option = requestOption();
+
         requestSkillName();
-        submitData();
+
+        if (option == 1) {
+            requestSkillDescription();
+            submitDataOption1();
+        } else {
+            submitDataElse();
+        }
+
 
     }
 
-    private void submitData() {
-        Optional<Skill> skill = getController().registerSkill(skillName);
+    /**
+     * Submits the skill data to the controller and displays a success or failure message.
+     */
+    private void submitDataElse() {
+        Optional<Skill> skill = getController().registerSkill(skillName, skillDescription);
 
+        // Verify
         if (skill.isPresent()) {
-            System.out.println("Job successfully registered!");
+            System.out.println("Skill successfully registered!");
         } else {
-            System.out.println("Job not registered!");
+            System.out.println("Skill not registered!");
         }
     }
 
+
+    /**
+     * Submits the skill data without description to the controller and displays a success or failure message.
+     */
+    private void submitDataOption1() {
+        Optional<Skill> skill = getController().registerSkill(skillName);
+
+        // Verify
+        if (skill.isPresent()) {
+            System.out.println("Skill successfully registered!");
+        } else {
+            System.out.println("Skill not registered!");
+        }
+    }
+
+    /**
+     * Requests the skill name from the user.
+     */
     private void requestSkillName() {
         Scanner input = new Scanner(System.in);
         System.out.print("Skill Name: ");
         skillName = input.nextLine();
     }
 
+    /**
+     * Requests the skill description from the user.
+     */
+    private void requestSkillDescription() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Skill Description: ");
+        skillDescription = input.nextLine();
+    }
+
+    /**
+     * Requests the option from the user for skill registration.
+     *
+     * @return The option selected by the user.
+     */
+    private int requestOption() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Select an option:\n");
+        System.out.print(" (1) - Create a skill (name and description) \n");
+        System.out.print(" (_) - Create a skill (name) \n");
+
+        while (true) {
+            try {
+                return input.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input for option selected. Please enter a valid option (1 or _):");
+                input.nextLine();
+            }
+        }
+    }
 }
