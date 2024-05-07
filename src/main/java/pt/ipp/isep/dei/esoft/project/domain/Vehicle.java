b11cee407764224907b1ca20f5714d42cbfba0a3
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-05-07 16:35:25 +0100

    US006 - Final Documentation.

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Vehicle.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Vehicle.java
@@ -3,6 +3,7 @@ package pt.ipp.isep.dei.esoft.project.domain;
 
 public class Vehicle {
 
+    // Attributes
     private String plateId;
     private String brand;
     private String model;
@@ -17,13 +18,30 @@ public class Vehicle {
     private Data registerDate;
     private Data acquisitionDate;
 
-
+    // Constants
     private static final int LOWEST_LIMIT = 1992;
     private static final int MIDDLE_LIMIT = 2005;
     private static final int HIGHEST_LIMIT = 2020;
     private static final int MAX_CHARS_PLATE = 8;
 
 
+
+    /**
+     * Constructs a Vehicle object with the given parameters.
+     *
+     * @param plateId          The plate ID of the vehicle.
+     * @param brand            The brand of the vehicle.
+     * @param model            The model of the vehicle.
+     * @param type             The type of the vehicle.
+     * @param tareWeight       The tare weight of the vehicle.
+     * @param grossWeight      The gross weight of the vehicle.
+     * @param currentKm        The current kilometers of the vehicle.
+     * @param checkUpFrequency The frequency of check-up for the vehicle.
+     * @param lastCheckUp      The last check-up kilometers of the vehicle.
+     * @param registerDate     The registration date of the vehicle.
+     * @param acquisitionDate  The acquisition date of the vehicle.
+     * @throws IllegalArgumentException if any parameter validation fails.
+     */
     public Vehicle(String plateId, String brand, String model, String type, float tareWeight, float grossWeight,
                    float currentKm, float checkUpFrequency, float lastCheckUp, Data registerDate, Data acquisitionDate) {
 
@@ -43,18 +61,39 @@ public class Vehicle {
 
     }
 
+
+    /**
+     * Returns the current kilometers of the vehicle.
+     *
+     * @return The current kilometers of the vehicle.
+     */
     public float getCurrentKm() {
         return currentKm;
     }
 
+    /**
+     * Returns the check-up frequency of the vehicle.
+     *
+     * @return The check-up frequency of the vehicle.
+     */
     public float getCheckUpFrequency() {
         return checkUpFrequency;
     }
 
+    /**
+     * Returns the last check-up kilometers of the vehicle.
+     *
+     * @return The last check-up kilometers of the vehicle.
+     */
     public float getLastCheckUp() {
         return lastCheckUp;
     }
 
+    /**
+     * Returns the plate ID of the vehicle.
+     *
+     * @return The plate ID of the vehicle.
+     */
     public String getPlateId() {
         return plateId;
     }
@@ -62,6 +101,24 @@ public class Vehicle {
 
     //---------------------------------------VALIDATIONS----------------------------------------------------------
 
+
+
+    /**
+     * Validates the parameters passed to the Vehicle constructor.
+     *
+     * @param plateId          The plate ID of the vehicle.
+     * @param brand            The brand of the vehicle.
+     * @param model            The model of the vehicle.
+     * @param type             The type of the vehicle.
+     * @param tareWeight       The tare weight of the vehicle.
+     * @param grossWeight      The gross weight of the vehicle.
+     * @param currentKm        The current kilometers of the vehicle.
+     * @param checkUpFrequency The frequency of check-up for the vehicle.
+     * @param lastCheckUp      The last check-up kilometers of the vehicle.
+     * @param registerDate     The registration date of the vehicle.
+     * @param acquisitionDate  The acquisition date of the vehicle.
+     * @throws IllegalArgumentException if any parameter validation fails.
+     */
     private void validateVehicle(String plateId, String brand, String model, String type, float tareWeight, float grossWeight, float currentKm, float checkUpFrequency, float lastCheckUp, Data registerDate, Data acquisitionDate) {
 
         //String validations and plate id format
@@ -77,6 +134,16 @@ public class Vehicle {
 
     }
 
+    /**
+     * Validates if any of the given floats are negative.
+     *
+     * @param tareWeight       The tare weight of the vehicle.
+     * @param grossWeight      The gross weight of the vehicle.
+     * @param lastCheckUp      The last check-up kilometers of the vehicle.
+     * @param currentKm        The current kilometers of the vehicle.
+     * @param checkUpFrequency The frequency of check-up for the vehicle.
+     * @throws IllegalArgumentException if any of the given floats are negative.
+     */
     private void validatesNegatives(float tareWeight, float grossWeight, float lastCheckUp, float currentKm, float checkUpFrequency) {
         if (isNegative(tareWeight)) {
             throw new IllegalArgumentException("Tare Weight cannot be negative!");
@@ -95,6 +162,14 @@ public class Vehicle {
         }
     }
 
+
+    /**
+     * Validates if the tare weight is not greater than the gross weight.
+     *
+     * @param tareWeight  The tare weight of the vehicle.
+     * @param grossWeight The gross weight of the vehicle.
+     * @throws IllegalArgumentException if the tare weight is greater than the gross weight.
+     */
     private void validatesWeights(float tareWeight, float grossWeight) {
         if (tareWeight > grossWeight) {
             throw new IllegalArgumentException("'Tare Weight' -> [" + tareWeight + "] cannot be bigger than 'Gross Weight' -> [" + grossWeight + "].");
@@ -102,13 +177,27 @@ public class Vehicle {
     }
 
 
-
+    /**
+     * Validates if the registration date is not later than the acquisition date.
+     *
+     * @param registerDate    The registration date of the vehicle.
+     * @param acquisitionDate The acquisition date of the vehicle.
+     * @throws IllegalArgumentException if the registration date is later than the acquisition date.
+     */
     private void validateDates(Data registerDate, Data acquisitionDate) {
         if (registerDate.isGreater(acquisitionDate)) {
             throw new IllegalArgumentException("'Acquisition Date' -> [" + acquisitionDate + "] cannot be latter than 'Register Date' -> [" + registerDate + "].");
         }
     }
 
+
+    /**
+     * Validates if the current kilometers are not less than the last check-up kilometers.
+     *
+     * @param currentKm  The current kilometers of the vehicle.
+     * @param lastCheckUp The last check-up kilometers of the vehicle.
+     * @throws IllegalArgumentException if the current kilometers are less than the last check-up kilometers.
+     */
     private void validateKm(float currentKm, float lastCheckUp) {
         if (currentKm < lastCheckUp) {
             throw new IllegalArgumentException("'Last Checkup' -> [" + lastCheckUp + "] cannot be bigger than 'Current Km' -> [" + currentKm + "].");
@@ -116,7 +205,13 @@ public class Vehicle {
 
     }
 
-
+    /**
+     * Validates the plate ID format and structure.
+     *
+     * @param plateId      The plate ID of the vehicle.
+     * @param registerDate The registration date of the vehicle.
+     * @throws IllegalArgumentException if the plate ID format or structure is invalid.
+     */
     private void validatePlateId(String plateId, Data registerDate) {
         if (!validateString(plateId)) {
             throw new IllegalArgumentException("PLATE ID cannot be null or empty -> [" + plateId + "].");
@@ -125,6 +220,12 @@ public class Vehicle {
     }
 
 
+    /**
+     * Validates the brand of the vehicle.
+     *
+     * @param brand The brand of the vehicle.
+     * @throws IllegalArgumentException if the brand is null, empty, contains special characters, or numbers.
+     */
     private void validateBrand(String brand) {
         if (!validateString(brand)) {
             throw new IllegalArgumentException("BRAND cannot be null or empty -> [" + brand + "].");
@@ -138,6 +239,13 @@ public class Vehicle {
 
     }
 
+
+    /**
+     * Validates the model of the vehicle.
+     *
+     * @param model The model of the vehicle.
+     * @throws IllegalArgumentException if the model is null, empty, or contains special characters.
+     */
     private void validateModel(String model) {
         if (!validateString(model)) {
             throw new IllegalArgumentException("Reference (MODEL) cannot be null or empty.");
@@ -148,6 +256,12 @@ public class Vehicle {
         }
     }
 
+    /**
+     * Validates the type of the vehicle.
+     *
+     * @param type The type of the vehicle.
+     * @throws IllegalArgumentException if the type is null, empty, contains special characters, or numbers.
+     */
     private void validateType(String type) {
         if (!validateString(type)) {
             throw new IllegalArgumentException("TYPE cannot be null or empty -> [" + type + "].");
@@ -163,10 +277,23 @@ public class Vehicle {
 
 
     //--------------------------------INTERNAL METHODS FOR VALIDATIONS--------------------------------
+    /**
+     * Validates if a string is not null or empty.
+     *
+     * @param string The string to be validated.
+     * @return true if the string is not null or empty, false otherwise.
+     */
     private boolean validateString(String string) {
         return (string != null && !string.trim().isEmpty());
     }
 
+
+    /**
+     * Validates if a string contains special characters.
+     *
+     * @param string The string to be validated.
+     * @return true if the string contains special characters, false otherwise.
+     */
     private boolean haveSpecialCharacters(String string) {
         for (int i = 0; i < string.length(); i++) {
             char c = string.charAt(i);
@@ -177,10 +304,24 @@ public class Vehicle {
         return false;
     }
 
+
+    /**
+     * Checks if a given float is negative.
+     *
+     * @param weight The float to be checked.
+     * @return true if the float is negative, false otherwise.
+     */
     private boolean isNegative(float weight) {
         return weight < 0;
     }
 
+
+    /**
+     * Validates if a string contains numbers.
+     *
+     * @param string The string to be validated.
+     * @return true if the string contains numbers, false otherwise.
+     */
     private boolean haveNumbers(String string) {
         for (int i = 0; i < string.length(); i++) {
             char c = string.charAt(i);
@@ -191,6 +332,14 @@ public class Vehicle {
         return false;
     }
 
+
+    /**
+     * Validates the format of the plate ID based on the registration date.
+     *
+     * @param plateId The plate ID of the vehicle.
+     * @param registerDate The registration date of the vehicle.
+     * @throws IllegalArgumentException if the plate ID format is invalid.
+     */
     private void validatePlate(String plateId, Data registerDate) {
 
         verifySizeOfPlate(plateId);
@@ -224,6 +373,15 @@ public class Vehicle {
 
     }
 
+
+    /**
+     * Validates the plate format for vehicles registered after the highest limit.
+     *
+     * @param first  The first part of the plate ID.
+     * @param second The second part of the plate ID.
+     * @param third  The third part of the plate ID.
+     * @throws IllegalArgumentException if the plate format is invalid for vehicles registered after the highest limit.
+     */
     private void validatePlateAfterHighest(String first, String second, String third) {
         if (first.matches("[A-Z]{2}") && second.matches("\\d{2}") && third.matches("[A-Z]{2}")) {
             // Plate matches "After 2020: AA-00-AA"
@@ -232,6 +390,15 @@ public class Vehicle {
         }
     }
 
+
+    /**
+     * Validates the plate format for vehicles registered between the middle and highest limit.
+     *
+     * @param first  The first part of the plate ID.
+     * @param second The second part of the plate ID.
+     * @param third  The third part of the plate ID.
+     * @throws IllegalArgumentException if the plate format is invalid for vehicles registered between the middle and highest limit.
+     */
     private void validatePlateAfterMiddle(String first, String second, String third) {
         if (first.matches("\\d{2}") && second.matches("[A-Z]{2}") && third.matches("\\d{2}")) {
             // Plate matches "Between 2005-2020: 00-AA-00"
@@ -241,6 +408,14 @@ public class Vehicle {
     }
 
 
+    /**
+     * Validates the plate format for vehicles registered between the lowest and middle limit.
+     *
+     * @param first  The first part of the plate ID.
+     * @param second The second part of the plate ID.
+     * @param third  The third part of the plate ID.
+     * @throws IllegalArgumentException if the plate format is invalid for vehicles registered between the lowest and middle limit.
+     */
     private void validatePlateAfterLowest(String first, String second, String third) {
         if (first.matches("\\d{2}") && second.matches("\\d{2}") && third.matches("[A-Z]{2}")) {
             // Plate matches "Between 1992-2005: 00-00-XX"
@@ -249,6 +424,13 @@ public class Vehicle {
         }
     }
 
+
+    /**
+     * Verifies the size of the plate ID.
+     *
+     * @param plateId The plate ID of the vehicle.
+     * @throws IllegalArgumentException if the plate ID size exceeds the maximum allowed length.
+     */
     private void verifySizeOfPlate(String plateId) {
         if (plateId.length() > MAX_CHARS_PLATE) {
             throw new IllegalArgumentException("PlateId is too long... MAX LENGTH -> [" + MAX_CHARS_PLATE + "]");
@@ -281,7 +463,7 @@ public class Vehicle {
     }
 
     /**
-     * Clone method.
+     * Clones the current instance of Vehicle.
      *
      * @return A clone of the current instance.
      */
@@ -289,6 +471,12 @@ public class Vehicle {
         return new Vehicle(plateId, brand, model, type, tareWeight, grossWeight, currentKm, checkUpFrequency, lastCheckUp, registerDate, acquisitionDate);
     }
 
+
+    /**
+     * Returns a string representation of the Vehicle object.
+     *
+     * @return A string representation of the object.
+     */
     @Override
     public String toString() {
         return String.format("• Plate: %s | Brand: %s | Model: %s | Current Km: %.2f%n" +
--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterVehicleUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/RegisterVehicleUI.java
@@ -10,6 +10,8 @@ import java.util.Scanner;
 
 import static pt.ipp.isep.dei.esoft.project.ui.console.ColorfulOutput.*;
 
+
+
 public class RegisterVehicleUI implements Runnable {
 
     private final VehicleController controller;
@@ -29,14 +31,27 @@ public class RegisterVehicleUI implements Runnable {
     private Data acquisitionDate;
 
 
+    /**
+     * Constructor for the class.
+     */
     public RegisterVehicleUI() {
         controller = new VehicleController();
     }
 
+
+    /**
+     * Retrieves the VehicleController instance.
+     *
+     * @return The VehicleController instance.
+     */
     private VehicleController getController() {
         return controller;
     }
 
+
+    /**
+     * Executes the user interface logic for registering a vehicle.
+     */
     public void run() {
         System.out.println("\n\n--- Register Vehicle ------------------------");
 
@@ -48,6 +63,11 @@ public class RegisterVehicleUI implements Runnable {
         }
 
     }
+
+
+    /**
+     * Executes the user interface logic for registering a vehicle in case of an exception.
+     */
     public void runException() {
 
         if (repeatProcess()) {
@@ -61,6 +81,12 @@ public class RegisterVehicleUI implements Runnable {
 
     }
 
+
+    /**
+     * Asks the user whether to repeat the registration process.
+     *
+     * @return True if the user wants to register a new vehicle, false otherwise.
+     */
     private boolean repeatProcess() {
         System.out.print("\nDo you wish to register new info? (y/n): ");
         Scanner scanner = new Scanner(System.in);
@@ -77,8 +103,9 @@ public class RegisterVehicleUI implements Runnable {
         }
     }
 
+
     /**
-     * Confirms the skill data entered by the user.
+     * Asks the user to confirm the entered vehicle data.
      *
      * @return 2 if the user chooses to exit, otherwise returns 1.
      */
@@ -106,8 +133,9 @@ public class RegisterVehicleUI implements Runnable {
         return option;
     }
 
+
     /**
-     * Displays the typed skill name and options for confirmation.
+     * Displays the typed vehicle information and options for confirmation.
      */
     private void display() {
         StringBuilder stringBuilder = new StringBuilder(String.format("Plate: %s | Brand: %s | Model: %s | Current Km: %.2f | Register Date: %s | Acquisition Date: %s", plateId, brand, model, currentKm, registerDate, acquisitionDate));
@@ -116,25 +144,46 @@ public class RegisterVehicleUI implements Runnable {
     }
 
 
-
+    /**
+     * Submits the entered vehicle data for registration.
+     *
+     *
+     * This method invokes the registration process for the vehicle using the
+     * provided data. It calls the corresponding method in the VehicleController
+     * to register the vehicle in the repository. If the registration is successful,
+     * it prints a success message. If the registration fails due to duplicate
+     * registration (i.e., the vehicle is already registered), it prints an error
+     * message.
+     *
+     * If an IllegalArgumentException occurs during the registration process,
+     * indicating invalid data, it catches the exception, prints the error message,
+     * and prompts the user to repeat the registration process.
+     */
     private void submitData() {
+        // Attempt to register the vehicle using the provided data
         try{
 
             Optional<Vehicle> vehicle = getController().registerVehicle(plateId, brand, model, type, tareWeight, grossWeight, currentKm, checkUpFrequency, lastCheckUp, registerDate, acquisitionDate);
 
+            // Check if the registration was successful
             if (vehicle.isPresent()) {
                 System.out.println(ANSI_BRIGHT_GREEN+"Vehicle successfully registered!"+ ANSI_RESET);
             } else {
+                // Print error message if vehicle is already registered
                 System.out.println(ANSI_BRIGHT_RED +"Vehicle not registered - Already registered!"+ ANSI_RESET);
             }
 
         }catch (IllegalArgumentException e){
+            // Catch IllegalArgumentException indicating invalid data
             System.out.println(ANSI_BRIGHT_RED +e.getMessage()+ANSI_RESET);
+            // Prompt user to repeat registration process
             runException();
         }
-
     }
 
+    /**
+     * Requests vehicle information from the user.
+     */
     private void requestVehicleInformation() {
 
         plateId = requestPlateID();
@@ -160,31 +209,57 @@ public class RegisterVehicleUI implements Runnable {
         acquisitionDate = requestAcquisitionDate();
     }
 
-
+    /**
+     * Requests the plate ID of the vehicle from the user.
+     *
+     * @return The plate ID entered by the user.
+     */
     private String requestPlateID() {
         Scanner input = new Scanner(System.in);
         System.out.print("Plate ID: ");
         return input.next();
     }
 
+
+    /**
+     * Requests the brand of the vehicle from the user.
+     *
+     * @return The brand entered by the user.
+     */
     private String requestBrand() {
         Scanner input = new Scanner(System.in);
         System.out.print("Brand: ");
         return input.next();
     }
 
+    /**
+     * Requests the model of the vehicle from the user.
+     *
+     * @return The model entered by the user.
+     */
     private String requestModel() {
         Scanner input = new Scanner(System.in);
         System.out.print("Model: ");
         return input.next();
     }
 
+    /**
+     * Requests the type of the vehicle from the user.
+     *
+     * @return The type entered by the user.
+     */
     private String requestType() {
         Scanner input = new Scanner(System.in);
         System.out.print("Type: ");
         return input.next();
     }
 
+
+    /**
+     * Requests the tare weight of the vehicle from the user.
+     *
+     * @return The tare weight entered by the user.
+     */
     private float requestTareWeight() {
         float resposta;
         Scanner input = new Scanner(System.in);
@@ -201,6 +276,11 @@ public class RegisterVehicleUI implements Runnable {
         }
     }
 
+    /**
+     * Requests the gross weight of the vehicle from the user.
+     *
+     * @return The gross weight entered by the user.
+     */
     private float requestGrossWeight() {
         float resposta;
         Scanner input = new Scanner(System.in);
@@ -217,6 +297,12 @@ public class RegisterVehicleUI implements Runnable {
         }
     }
 
+
+    /**
+     * Requests the current kilometers of the vehicle from the user.
+     *
+     * @return The current kilometers entered by the user.
+     */
     private float requestCurrentKm() {
         float resposta;
         Scanner input = new Scanner(System.in);
@@ -227,12 +313,23 @@ public class RegisterVehicleUI implements Runnable {
                 resposta = input.nextFloat();
                 return resposta;
             } catch (InputMismatchException e) {
+                // Clear the invalid input
                 System.out.print(ANSI_BRIGHT_RED + "Invalid Current Kilometers! Enter a new one: " + ANSI_RESET);
+                // Prompt the user again for a valid input
                 input.nextLine();
             }
         }
     }
 
+
+    /**
+     * Prompts the user to input the frequency of check-ups in kilometers.
+     * <p>
+     * This method continuously prompts the user to enter the frequency
+     * of check-ups in kilometers until a valid floating-point number is provided.
+     *
+     * @return The frequency of check-ups in kilometers input by the user.
+     */
     private float requestCheckUpFrequency() {
         float resposta;
         Scanner input = new Scanner(System.in);
@@ -249,6 +346,15 @@ public class RegisterVehicleUI implements Runnable {
         }
     }
 
+
+    /**
+     * Prompts the user to input the distance traveled since the last check-up.
+     * <p>
+     * This method continuously prompts the user to enter the distance
+     * traveled since the last check-up until a valid floating-point number is provided.
+     *
+     * @return The distance traveled since the last check-up input by the user.
+     */
     private float requestLastCheckUp() {
         float resposta;
         Scanner input = new Scanner(System.in);
@@ -264,11 +370,30 @@ public class RegisterVehicleUI implements Runnable {
         }
     }
 
+
+    /**
+     * Prompts the user to input the registration date.
+     * <p>
+     * This method prompts the user to input the registration date
+     * by asking for the year, month, and day separately. It handles
+     * any invalid inputs gracefully and keeps prompting until a valid date is provided.
+     *
+     * @return The registration date input by the user.
+     */
     private Data requestRegisterDate() {
         System.out.print("\n-- Register date --\n");
         return getData();
     }
 
+
+    /**
+     * Prompts the user to input the acquisition date.
+     * This method prompts the user to input the acquisition date
+     * by asking for the year, month, and day separately. It handles
+     * any invalid inputs gracefully and keeps prompting until a valid date is provided.
+     *
+     * @return The acquisition date input by the user.
+     */
     private Data requestAcquisitionDate() {
         System.out.print("-- Acquisition date --\n");
         return getData();
@@ -276,6 +401,14 @@ public class RegisterVehicleUI implements Runnable {
 
 
     //------------------INPUTS DATAS-------------------------------
+    /**
+     * Prompts the user to input a date (year, month, and day).
+     * This method prompts the user to input a date by asking for
+     * the year, month, and day separately. It handles any invalid inputs
+     * gracefully and keeps prompting until a valid date is provided.
+     *
+     * @return The date input by the user.
+     */
     private Data getData() {
         Data data;
         while (true) {
@@ -291,6 +424,14 @@ public class RegisterVehicleUI implements Runnable {
         }
     }
 
+    /**
+     * Prompts the user to input a day.
+     * This method prompts the user to input a day as an integer.
+     * It handles any invalid inputs gracefully and keeps prompting
+     * until a valid day is provided.
+     *
+     * @return The day input by the user.
+     */
     private int requestDay() {
         int resposta;
         Scanner input = new Scanner(System.in);
@@ -306,6 +447,15 @@ public class RegisterVehicleUI implements Runnable {
         }
     }
 
+
+    /**
+     * Prompts the user to input a month.
+     * This method prompts the user to input a month as an integer.
+     * It handles any invalid inputs gracefully and keeps prompting
+     * until a valid month is provided.
+     *
+     * @return The month input by the user.
+     */
     private int requestMonth() {
         int resposta;
         Scanner input = new Scanner(System.in);
@@ -321,6 +471,14 @@ public class RegisterVehicleUI implements Runnable {
         }
     }
 
+    /**
+     * Prompts the user to input a year.
+     * This method prompts the user to input a year as an integer.
+     * It handles any invalid inputs gracefully and keeps prompting
+     * until a valid year is provided.
+     *
+     * @return The year input by the user.
+     */
     private int requestYear() {
         int resposta;
         Scanner input = new Scanner(System.in);
