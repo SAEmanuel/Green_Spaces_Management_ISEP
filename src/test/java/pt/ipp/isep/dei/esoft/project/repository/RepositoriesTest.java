291c5655bffd55e81bde26e48164e8e9801be511
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-05-07 17:39:47 +0100

    Repositories Test

--- src/test/java/pt/ipp/isep/dei/esoft/project/repository/RepositoriesTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/repository/RepositoriesTest.java
@@ -23,4 +23,48 @@ class RepositoriesTest {
         Repositories instance = Repositories.getInstance();
         assertNotNull(instance.getTaskCategoryRepository());
     }
+
+    @Test
+    void getAuthenticationRepository() {
+        Repositories instance = Repositories.getInstance();
+        assertNotNull(instance.getAuthenticationRepository());
+    }
+
+    @Test
+    void getJobRepository() {
+        Repositories instance = Repositories.getInstance();
+        assertNotNull(instance.getJobRepository());
+    }
+
+    @Test
+    void getSkillRepository() {
+        Repositories instance = Repositories.getInstance();
+        assertNotNull(instance.getSkillRepository());
+    }
+
+    @Test
+    void getVehicleRepository() {
+        Repositories instance = Repositories.getInstance();
+        assertNotNull(instance.getVehicleRepository());
+    }
+
+    @Test
+    void getCollaboratorRepository() {
+        Repositories instance = Repositories.getInstance();
+        assertNotNull(instance.getCollaboratorRepository());
+    }
+
+    @Test
+    void getTeamRepository() {
+        Repositories instance = Repositories.getInstance();
+        assertNotNull(instance.getTeamRepository());
+    }
+
+    @Test
+    void getVehicleCheckUpRepository() {
+        Repositories instance = Repositories.getInstance();
+        assertNotNull(instance.getVehicleCheckUpRepository());
+    }
+
+
 }
---
+++ src/test/java/pt/ipp/isep/dei/esoft/project/repository/SkillRepositoryTest.java
@@ -0,0 +1,21 @@
+package pt.ipp.isep.dei.esoft.project.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SkillRepositoryTest {
+
+    @Test
+    void registerSkill() {
+    }
+
+    @Test
+    void getSkillList() {
+    }
+
+    @Test
+    void testClone() {
+    }
+
+}
