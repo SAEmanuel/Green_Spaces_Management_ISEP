27b8d50b955acf8dcd8cdded10b592ec711c499d
1231498 <1231498@isep.ipp.pt>
2024-05-31 16:22:38 +0100

    US027 - Added 2 Descending order methods

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/SortGreenSpaces.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/SortGreenSpaces.java
@@ -26,12 +26,23 @@ public class SortGreenSpaces {
                 String value = config.getProperty(key);
 
                 if(value.equalsIgnoreCase(sortingMethod)){
-                    if (value.equalsIgnoreCase("ascending")) {
-                        greenSpaces = ascending(greenSpaces);
-                        sorted= true;
-                    }else if (value.equalsIgnoreCase("descending")) {
-                        greenSpaces = descending(greenSpaces);
-                        sorted= true;
+                    switch (sortingMethod){
+                        case "Ascending By Name":
+                            greenSpaces = ascendingName(greenSpaces);
+                            sorted= true;
+                            break;
+                        case "Descending By Name":
+                            greenSpaces = descendingName(greenSpaces);
+                            sorted= true;
+                            break;
+                        case "Bubble Sort":
+                            greenSpaces = bubbleSort(greenSpaces);
+                            sorted= true;
+                            break;
+                        case "Insertion Sort":
+                            greenSpaces = insertionSort(greenSpaces);
+                            sorted= true;
+                            break;
                     }
                 }
             }
@@ -45,15 +56,50 @@ public class SortGreenSpaces {
         }
     }
 
-    private static List<GreenSpace> ascending(List<GreenSpace> greenSpaces) {
+    private static List<GreenSpace> ascendingName(List<GreenSpace> greenSpaces) {
         Collections.sort(greenSpaces, Comparator.comparing(GreenSpace::getName));
         return greenSpaces;
     }
 
     // Example sorting method
-    private static List<GreenSpace> descending(List<GreenSpace> greenSpaces) {
+    private static List<GreenSpace> descendingName(List<GreenSpace> greenSpaces) {
         Collections.sort(greenSpaces, Comparator.comparing(GreenSpace::getName).reversed());
         return greenSpaces;
+    }
+
+    private static List<GreenSpace> bubbleSort(List<GreenSpace> greenSpaces) {
+        List<GreenSpace> newGreenSpacesList = new ArrayList<>(greenSpaces);
+        int n = newGreenSpacesList.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (newGreenSpacesList.get(j).getArea() < newGreenSpacesList.get(j + 1).getArea()) {
+                    // Swap greenSpaces[j] and greenSpaces[j+1] for descending order
+                    GreenSpace temp = newGreenSpacesList.get(j);
+                    newGreenSpacesList.set(j, newGreenSpacesList.get(j + 1));
+                    newGreenSpacesList.set(j + 1, temp);
+                }
+            }
+        }
+        return newGreenSpacesList;
+    }
+
+    private static List<GreenSpace> insertionSort(List<GreenSpace> greenSpaces) {
+        List<GreenSpace> newGreenSpacesList = new ArrayList<>(greenSpaces);
+        int n = newGreenSpacesList.size();
+        for (int i = 1; i < n; ++i) {
+            GreenSpace key = newGreenSpacesList.get(i);
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1], that are greater than key, to one position ahead
+            // of their current position
+            while (j >= 0 && newGreenSpacesList.get(j).getArea() < key.getArea()) {
+                newGreenSpacesList.set(j + 1, newGreenSpacesList.get(j));
+                j = j - 1;
+            }
+            newGreenSpacesList.set(j + 1, key);
+        }
+
+        return newGreenSpacesList;
     }
 
     public static List<String> getSortTypes() throws IOException {
