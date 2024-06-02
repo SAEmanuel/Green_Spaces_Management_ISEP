c9a50e7f254c6677e440e7f4312746a8ecd3086f
RomeuZouXu <francpok.zouxu4@gmail.com>
2024-06-02 17:25:22 +0100

    AgendaRepositoryTest update v3

--- src/test/java/pt/ipp/isep/dei/esoft/project/repository/AgendaRepositoryTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/repository/AgendaRepositoryTest.java
@@ -72,4 +72,40 @@ public class AgendaRepositoryTest {
         boolean result = taskList.contains(agenda);
         assertTrue(result);
     }
+
+    @Test
+    public void testGetTaskPlannedList() {
+        List<AgendaEntry> plannedList = new ArrayList<>();
+        Team team = new Team(1);
+        Team team2 = new Team(2);
+        Job job = new Job("Marketing Coordinator");
+        Collaborator collaborator = new Collaborator("Francisco", new Data(2004, 5, 20), new Data(2023, 6, 12), "Rua da pedra", 912809789, "xico@gmail.com", 123456744, 0, "123456711", job, new Password("AAA12ab"), "xico@gmail.com");
+        Collaborator collaborator2 = new Collaborator("Emanuel", new Data(2004, 4, 20), new Data(2023, 6, 12), "Rua da Mariana", 912809777, "ema@gmail.com", 123456755, 0, "123456722", job, new Password("AAA12ac"), "ema@gmail.com");
+        team.addCollaborator(collaborator);
+        team2.addCollaborator(collaborator2);
+        GreenSpace greenSpace = new GreenSpace("Infante Dom Henrique", 0, 100, "Porto", "gsm@this.app");
+        ToDoEntry agendaEntry = new ToDoEntry(greenSpace, "Cortar relva", "description", 0, 8);
+        AgendaEntry agenda = new AgendaEntry(agendaEntry, new Data(2025, 2, 2));
+        agenda.setTeam(team);
+        GreenSpace greenSpace2 = new GreenSpace("Palacio de Cristal", 1, 40, "Porto", "gsm@this.app");
+        ToDoEntry agendaEntry2 = new ToDoEntry(greenSpace2, "Cortar relva", "description", 0, 8);
+        AgendaEntry agenda2 = new AgendaEntry(agendaEntry2, new Data(2025, 3, 2));
+        agenda2.setTeam(team2);
+        Data startDate = new Data(2022, 1, 1);
+        Data endDate = new Data(2025,3,1);
+        if (agenda.getTeam().hasCollaborator(collaborator)
+                && agenda.getStartingDate().isGreaterOrEquals(startDate)
+                && !agenda.getStartingDate().isGreater(endDate)
+                && agenda.getAgendaEntry().getStatus().equals("Planned")) {
+            plannedList.add(agenda);
+        }
+        if (agenda2.getTeam().hasCollaborator(collaborator)
+                && agenda2.getStartingDate().isGreaterOrEquals(startDate)
+                && !agenda2.getStartingDate().isGreater(endDate)
+                && agenda2.getAgendaEntry().getStatus().equals("Planned")) {
+            plannedList.add(agenda2);
+        }
+        assertTrue(plannedList.contains(agenda));
+        assertFalse(plannedList.contains(agenda2));
+    }
 }
