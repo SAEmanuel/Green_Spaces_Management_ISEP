93c868e637bc3c53c8c38cbce6e053e04147bd2f
Jorge Ubaldo <1231274@isep.ipp.pt>
2024-06-02 12:56:01 +0100

    General Documentation v3.1.0

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/ToDoListRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/ToDoListRepository.java
@@ -7,26 +7,56 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repository class for managing a list of ToDoEntry objects.
+ */
 public class ToDoListRepository implements Serializable {
     private final List<ToDoEntry> toDoList;
 
+    /**
+     * Constructor for ToDoListRepository.
+     */
     public ToDoListRepository() {
-       toDoList = new ArrayList<>();
+        toDoList = new ArrayList<>();
     }
 
+    /**
+     * Retrieves the list of ToDoEntry objects.
+     *
+     * @return the list of ToDoEntry objects.
+     */
     public List<ToDoEntry> getToDoList() {
         return toDoList;
     }
 
-    public List<ToDoEntry> getToDoListCLone() {
+    /**
+     * Retrieves a cloned list of ToDoEntry objects.
+     *
+     * @return a clone of the list of ToDoEntry objects.
+     */
+    public List<ToDoEntry> getToDoListClone() {
         return clone();
     }
 
+    /**
+     * Creates a clone of the current list of ToDoEntry objects.
+     *
+     * @return a new list containing the cloned ToDoEntry objects.
+     */
     public List<ToDoEntry> clone() {
         return new ArrayList<>(this.toDoList);
     }
 
-
+    /**
+     * Registers a new ToDoEntry object and adds it to the repository.
+     *
+     * @param greenSpace       the green space associated with the entry.
+     * @param title            the title of the entry.
+     * @param description      the description of the entry.
+     * @param urgency          the urgency level of the entry.
+     * @param expectedDuration the expected duration of the entry.
+     * @return an Optional containing the registered ToDoEntry object, or empty if registration fails.
+     */
     public Optional<ToDoEntry> registerToDoEntry(GreenSpace greenSpace, String title, String description, int urgency, int expectedDuration) {
         Optional<ToDoEntry> optionalToDoEntry = Optional.empty();
 
@@ -39,14 +69,24 @@ public class ToDoListRepository implements Serializable {
         return optionalToDoEntry;
     }
 
-    public void add(ToDoEntry entry){
+    /**
+     * Adds an entry to the to-do list.
+     *
+     * @param entry the ToDoEntry object to be added.
+     */
+    public void add(ToDoEntry entry) {
         toDoList.add(entry);
     }
 
+    /**
+     * Adds a ToDoEntry object to the repository if it passes validation.
+     *
+     * @param toDoEntry the ToDoEntry object to be added.
+     * @return true if the entry is successfully added, false otherwise.
+     */
     private boolean addToDoEntry(ToDoEntry toDoEntry) {
         boolean success = false;
 
-        // Validate the Check-Up before adding
         if (validate(toDoEntry)) {
             success = toDoList.add(toDoEntry);
         }
@@ -54,18 +94,41 @@ public class ToDoListRepository implements Serializable {
         return success;
     }
 
+    /**
+     * Validates a ToDoEntry object before adding it to the repository.
+     *
+     * @param toDoEntry the ToDoEntry object to be validated.
+     * @return true if the entry is valid, false otherwise.
+     */
     private boolean validate(ToDoEntry toDoEntry) {
         return toDoListDoesNotContainToDoEntry(toDoEntry);
     }
 
+    /**
+     * Checks if the repository does not already contain the specified ToDoEntry object.
+     *
+     * @param toDoEntry the ToDoEntry object to be checked.
+     * @return true if the repository does not contain the entry, false otherwise.
+     */
     private boolean toDoListDoesNotContainToDoEntry(ToDoEntry toDoEntry) {
         return !toDoList.contains(toDoEntry);
     }
 
+    /**
+     * Retrieves the urgency levels available for ToDoEntry objects.
+     *
+     * @return an array of Urgency enums.
+     */
     public ToDoEntry.Urgency[] getUrgency() {
         return ToDoEntry.Urgency.values();
     }
 
+    /**
+     * Retrieves a list of ToDoEntry objects for the specified responsible person.
+     *
+     * @param responsible the responsible person.
+     * @return a list of ToDoEntry objects assigned to the specified person.
+     */
     public List<ToDoEntry> getToDoListForResponsible(String responsible) {
         List<ToDoEntry> entry = new ArrayList<>();
 
@@ -76,6 +139,4 @@ public class ToDoListRepository implements Serializable {
         }
         return entry;
     }
-
-
 }
--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/AgendaUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/AgendaUI.java
@@ -14,25 +14,32 @@ import java.util.Scanner;
 import static pt.ipp.isep.dei.esoft.project.ui.console.ColorfulOutput.*;
 
 public class AgendaUI implements Runnable {
-    
-    private final AgendaController controller;
 
+    private final AgendaController controller;
     private ToDoEntryDTO agendaEntry;
     private int toDoEntryOption;
     private Data starting_Date;
 
-
-    public AgendaUI(){
+    /**
+     * Constructor for AgendaUI.
+     */
+    public AgendaUI() {
         controller = new AgendaController();
     }
 
+    /**
+     * Gets the controller for managing agenda operations.
+     *
+     * @return the AgendaController instance.
+     */
     private AgendaController getController() {
         return controller;
     }
 
-
-
-    //------------------------------------ Run UI ---------------------------------------
+    /**
+     * Entry point to run the agenda UI operations.
+     */
+    @Override
     public void run() {
         System.out.println("\n\n--- Add Agenda Entry ------------------------");
 
@@ -43,12 +50,15 @@ public class AgendaUI implements Runnable {
                 submitData();
             }
         } else {
-            System.out.println(ANSI_BRIGHT_RED+ "Does not exist task in the TODO list... Add some"+ANSI_RESET);
+            System.out.println(ANSI_BRIGHT_RED + "Does not exist task in the TODO list... Add some" + ANSI_RESET);
         }
-
     }
 
-
+    /**
+     * Displays the confirmation menu and handles the user's choice.
+     *
+     * @return the chosen option.
+     */
     private int confirmsData() {
         int option;
         display();
@@ -67,36 +77,37 @@ public class AgendaUI implements Runnable {
         }
     }
 
-
+    /**
+     * Displays the current data entered by the user.
+     */
     private void display() {
-        System.out.printf("\nTyped data -> [%s]\n", String.format(ANSI_GREEN+"Task: "+ ANSI_RESET+"%s |" +ANSI_GREEN+ " Starting Date: "+ANSI_RESET+"%s", "\"" + agendaEntry.getObjDto().getTitle() + "\" in \"" + agendaEntry.getObjDto().getGreenSpace().getName() + "\"", starting_Date));
+        System.out.printf("\nTyped data -> [%s]\n", String.format(ANSI_GREEN + "Task: " + ANSI_RESET + "%s |" + ANSI_GREEN + " Starting Date: " + ANSI_RESET + "%s", "\"" + agendaEntry.getObjDto().getTitle() + "\" in \"" + agendaEntry.getObjDto().getGreenSpace().getName() + "\"", starting_Date));
         System.out.print("Confirmation menu:\n 0 -> Change Entry Info\n 1 -> Continue\n 2 -> Exit\nSelected option: ");
     }
 
-
+    /**
+     * Submits the entered data to register an agenda entry.
+     */
     private void submitData() {
-        // Attempt to register the vehicle using the provided data
         try {
-
             Optional<AgendaEntry> entry = getController().registerAgendaEntry(toDoEntryOption, starting_Date);
 
-            // Check if the registration was successful
             if (entry.isPresent()) {
                 System.out.println(ANSI_BRIGHT_GREEN + "Agenda entry successfully added!" + ANSI_RESET);
             } else {
-                // Print error message if vehicle is already registered
                 System.out.println(ANSI_BRIGHT_RED + "Agenda entry not added - Already exist!" + ANSI_RESET);
             }
 
         } catch (IllegalArgumentException e) {
-            // Catch IllegalArgumentException indicating invalid data
             System.out.println(ANSI_BRIGHT_RED + e.getMessage() + ANSI_RESET);
-            // Prompt user to repeat registration process
         }
     }
-    //------------------------------------------------------------------------------------
 
-    //--------------------------------------- Request Inputs ------------------------------------------
+    /**
+     * Requests entry information from the user.
+     *
+     * @return true if entry information is valid, false otherwise.
+     */
     private boolean requestEntryInformation() {
         toDoEntryOption = requestAgendaEntry();
         if (toDoEntryOption != -1) {
@@ -104,14 +115,23 @@ public class AgendaUI implements Runnable {
             return true;
         }
         return false;
-        
     }
 
+    /**
+     * Requests the starting date from the user.
+     *
+     * @return the starting date entered by the user.
+     */
     private Data requestStartDate() {
         System.out.print("-- Starting Date --\n");
         return GetDatesFromUsers.getData();
     }
 
+    /**
+     * Requests the agenda entry option from the user.
+     *
+     * @return the chosen agenda entry option, or -1 if no entries exist.
+     */
     private int requestAgendaEntry() {
         int answer;
         List<ToDoEntryDTO> toDoList = controller.getToDoListDTOForResponsible();
@@ -125,12 +145,13 @@ public class AgendaUI implements Runnable {
         }
         return -1;
     }
-    //------------------------------------------------------------------------------------
 
-
-
-
-    //-------------------------------- Other Methods -------------------------------------
+    /**
+     * Gets the user's answer for the agenda entry option.
+     *
+     * @param n the number of available options.
+     * @return the chosen option.
+     */
     private int getAnswer(int n) {
         int answer;
         Scanner input = new Scanner(System.in);
@@ -142,21 +163,24 @@ public class AgendaUI implements Runnable {
                     return answer;
                 }
             } catch (InputMismatchException e) {
-                System.out.print(ANSI_BRIGHT_RED + "Invalid ID number! Enter a new one: " + ANSI_RESET +"\n");
+                System.out.print(ANSI_BRIGHT_RED + "Invalid ID number! Enter a new one: " + ANSI_RESET + "\n");
                 input.nextLine();
             }
-            System.out.print(ANSI_BRIGHT_YELLOW+"Invalid ID, enter a new one: "+ANSI_RESET);
+            System.out.print(ANSI_BRIGHT_YELLOW + "Invalid ID, enter a new one: " + ANSI_RESET);
         }
     }
 
+    /**
+     * Displays the list of to-do entries.
+     *
+     * @param toDoList the list of to-do entries.
+     */
     private void showToDoList(List<ToDoEntryDTO> toDoList) {
         System.out.println("\n-- TODO List --");
         System.out.println("\n[Title -> Description -> Urgency -> Spectated duration -> Park]\n");
         for (int i = 0; i < toDoList.size(); i++) {
-            System.out.println("• Task: " + toDoList.get(i).toString() + "\n" + ANSI_PURPLE + "   Option -> [" + (i+1) + "]\n" + ANSI_RESET);
+            System.out.println("• Task: " + toDoList.get(i).toString() + "\n" + ANSI_PURPLE + "   Option -> [" + (i + 1) + "]\n" + ANSI_RESET);
         }
         System.out.println("----------------");
     }
-    //------------------------------------------------------------------------------------
-
 }
