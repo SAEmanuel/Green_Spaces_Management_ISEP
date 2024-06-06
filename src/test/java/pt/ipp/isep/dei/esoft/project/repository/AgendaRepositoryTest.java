8f484c1b08a7bafc012dd2698118d268a71d569f
RomeuZouXu <francpok.zouxu4@gmail.com>
2024-06-06 22:40:42 +0100

    AgendaRepositoryTest testGetPossibleDoneTaskPlannedList

--- src/test/java/pt/ipp/isep/dei/esoft/project/repository/AgendaRepositoryTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/repository/AgendaRepositoryTest.java
@@ -109,7 +109,7 @@ public class AgendaRepositoryTest {
     }
 
     @Test
-    public void testGetTaskPlannedList () {
+    public void testGetTaskPlannedList() {
         Repositories repositories = Repositories.getInstance();
         Job job1 = new Job("Jardineiro");
         Collaborator collaborator = new Collaborator("Francisco", new Data(2004, 7, 6), new Data(2023, 6, 12), "Rua da pedra", 912809789, "xico@gmail.com", 123456744, 0, "123456711", job1, new Password("AAA12ab"), "xico@gmail.com");
@@ -134,4 +134,29 @@ public class AgendaRepositoryTest {
         assertEquals(new Data(2023, 6, 5), result.get(0).getStartingDate());
     }
 
+    @Test
+    public void testGetPossibleDoneTaskPlannedList() {
+        Repositories repositories = Repositories.getInstance();
+        Job job1 = new Job("Jardineiro");
+        Collaborator collaborator = new Collaborator("Francisco", new Data(2004, 7, 6), new Data(2023, 6, 12), "Rua da pedra", 912809789, "xico@gmail.com", 123456744, 0, "123456711", job1, new Password("AAA12ab"), "xico@gmail.com");
+        GreenSpace greenSpace = new GreenSpace("Test Green Space", 0, 100, "Test City", "test@greenspace.com");
+        ToDoEntry toDoEntry = new ToDoEntry(greenSpace, "Work", "description", 1, 8);
+        ToDoEntry toDoEntry2 = new ToDoEntry(greenSpace, "Work", "description", 1, 8);
+        Team team = new Team(1);
+        Team team2 = new Team(2);
+        team.addCollaborator(collaborator);
+        team2.addCollaborator(collaborator);
+        toDoEntry.setStatus("Planned");
+        toDoEntry2.setStatus("Canceled");
+        AgendaEntry agendaEntry = new AgendaEntry(toDoEntry, new Data(2023, 6, 5));
+        AgendaEntry agendaEntry2 = new AgendaEntry(toDoEntry, new Data(2023, 7, 1));
+        agendaEntry.addTeam(team);
+        agendaEntry2.addTeam(team2);
+        repositories.getAgenda().addAgendaEntry(agendaEntry);
+        repositories.getAgenda().addAgendaEntry(agendaEntry2);
+
+        List<AgendaEntry> result = repositories.getAgenda().getTaskPlannedList(collaborator, new Data(2023, 6, 1), new Data(2023, 6, 30), 2);
+        assertEquals(1, result.size());
+        assertEquals(new Data(2023, 6, 5), result.get(0).getStartingDate());
+    }
 }
